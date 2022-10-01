22e1049d1e229942252d7d3461ab10845cb6563f
João Cabral Périgo <joao.perigo@devio.com.br>
2022-10-01 15:20:54 -0300

    ++toto

--- menu.php
+++ menu.php
@@ -1,18 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-light"> 
-<div class="container"> 
-<a class="navbar-brand" href="">...</a> 
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
-<span class="navbar-toggler-icon"></span> 
-</button> 
-<div class="collapse navbar-collapse" id="navbarSupportedContent"> <?php wp_nav_menu( 
-   array( 
-        'theme_location' => 'primary', 
-        'menu_id' => 'primary-menu', 
-        'container_class'=> 'ms-auto ', 
-        'menu_class'=>'navbar-nav' 
-    ) 
-); ?> 
-<!-- other nav ul lists --> 
-</div> 
-</div> 
+    <div class="container"> 
+        <a class="navbar-brand" href="">...</a> 
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
+            <span class="navbar-toggler-icon"></span> 
+        </button> 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent"> 
+            <?php wp_nav_menu( 
+                array( 
+                        'theme_location' => 'primary', 
+                        'menu_id' => 'primary-menu', 
+                        'container_class'=> 'ms-auto ', 
+                        'menu_class'=>'navbar-nav' 
+                    ) 
+            ); ?> 
+        <!-- other nav ul lists --> 
+        </div> 
+    </div> 
 </nav> 
